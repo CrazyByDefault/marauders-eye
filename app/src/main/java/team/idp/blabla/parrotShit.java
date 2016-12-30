f2f0dc49fbc0a9b8ccab3891b95ebb9627e4b299
CrazyByDefault <es16btech11025@iith.ac.in>
2016-12-30 14:18:01 +0530

    initial commit

---
+++ app/src/main/java/team/idp/blabla/parrotShit.java
@@ -0,0 +1,13 @@
+package team.idp.blabla;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class parrotShit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parrot_shit);
+    }
+}
