89ce7b816827401c56914320c499f4b8174be8eb
gitu-m <cs16btech11014@iith.ac.in>
2017-01-04 17:33:04 -1000

    Shared Preferences

--- app/src/main/java/team/idp/blabla/YouOnlyLiveOnce.java
+++ app/src/main/java/team/idp/blabla/YouOnlyLiveOnce.java
@@ -1,6 +1,8 @@
 package team.idp.blabla;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,8 +13,8 @@ public class YouOnlyLiveOnce extends AppCompatActivity {
 
     private Button doneBt;
     private EditText rollSpeedInput, timeInput, yawInput, waitInput, yawInc, gazInput;
-    private float time, wait, yawIncInt;
-    private int yaw, roll, gaz;
+    private float time = 0, wait = 0, yawIncInt = 0;
+    private int yaw = 0, roll = 0, gaz = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +28,23 @@ public class YouOnlyLiveOnce extends AppCompatActivity {
         waitInput = (EditText) findViewById(R.id.waitTimeInput);
         yawInc = (EditText) findViewById(R.id.yawStep);
         doneBt = (Button) findViewById(R.id.yoloDoneBt);
+
+        SharedPreferences settings = this.getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putFloat("Saved_time" ,time);
+        editor.putFloat("Saved_wait" ,wait);
+        editor.putInt("Saved_yaw" ,yaw);
+        editor.putFloat("Saved_yawInc", yawIncInt);
+        editor.putInt("Saved_roll", roll);
+        editor.putInt("Saved_gaz", gaz);
+        editor.commit();
+
+        yawInput.setText(settings.getInt("Saved_yaw", 0));
+        timeInput.setText(Float.toString(settings.getFloat("Saved_time", 0)));
+        rollSpeedInput.setText(Float.toString(settings.getFloat("Saved_wait", 0)));
+        waitInput.setText(Float.toString(settings.getFloat("Saved_yawInc", 0)));
+        rollSpeedInput.setText(settings.getInt("Saved_roll", 0));
+        gazInput.setText(settings.getInt("Saved_gaz", 0));
 
         doneBt.setOnClickListener(new View.OnClickListener() {
             @Override
