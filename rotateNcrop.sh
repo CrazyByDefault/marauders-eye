203df520a0e57df5d8279c4618ca497f2aa45043
CrazyByDefault <es16btech11025@iith.ac.in>
2017-01-24 20:21:53 +0530

    rotateNcrop file path modified again

--- rotateNcrop.sh
+++ rotateNcrop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for file in /media/Parrot_Mini/Airborne_Night/media/*.jpg; 
+for file in /media/shashank/Parrot_Mini/Airborne_Night/media/*.jpg; 
 do
 convert "$file" -rotate 180 -crop +0-165 "${file%.jpg}"_cropNrotate.jpg
 done
